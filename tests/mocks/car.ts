c4047775639e82bde3fd03110017bb1b59582471
rodpedrassi <rodpedrassi@gmail.com>
2023-02-14 10:53:31 -0300

    help no slack

---
+++ tests/mocks/car.ts
@@ -0,0 +1,21 @@
+const createCar = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const createCarRes = {
+  id: 'ajfnjian189023adon',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export default { createCar, createCarRes };
---
+++ tests/unit/Services/createCar.test.ts
@@ -0,0 +1,16 @@
+import { Model } from 'mongoose';
+import { expect } from 'chai';
+import Sinon from 'sinon';
+import car from '../../mocks/car';
+import CarService from '../../../src/Services/CarService';
+
+describe('Deveria criar um carro', function () {
+  test('Criando um carro com SUCESSO', async function () { 
+    Sinon.stub(Model, 'create').resolves(car.createCar);
+
+    const service = new CarService();
+    const result = await service.create(car.createCar);
+
+    expect(result).to.be.deep.equal(car.createCarRes);
+  });
+});
